ae47d1330bbbd90367cdc9d6ccef6b447266b237
puyup <hellopuyup@gmail.com>
2022-09-27 16:56:15 +0700

    move core templates folder

--- core/views/home.py
+++ core/views/home.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 class HomeView(View):
 	context = {}
-	template_name = 'home.html'
+	template_name = 'core/home.html'
 
 	def get(self, request, *args, **kwargs):
 		return render(request, self.template_name, self.context)
