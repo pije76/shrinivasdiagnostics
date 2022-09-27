383841466162e67fd251e89532f0547c65e85440
dj4va <dj4va@yahoo.com>
2022-09-27 15:33:44 +0700

    edit urls

--- shrinivasdiagnostics/urls.py
+++ shrinivasdiagnostics/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
     # path('accounts/', include('accounts.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
