c7e006d6facf8677a5b5b37079b38e78779ff354
dj4va <dj4va@yahoo.com>
2022-10-02 18:23:19 +0700

    edit accounts

--- accounts/managers.py
+++ accounts/managers.py
@@ -9,26 +9,35 @@ class UserManager(BaseUserManager):
     use_in_migrations = True
 
     # def _create_user(self, email, password, **extra_fields):
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        now = timezone.now()
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        # user = self.model(email=email, **extra_fields)
-        user = self.model(email=email, is_staff=False, is_active=True, is_superuser=False, last_login=now, date_joined=now, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+    # def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+    #     """
+    #     Creates and saves a User with the given email and password.
+    #     """
+    #     now = timezone.now()
+    #     if not email:
+    #         raise ValueError('The given email must be set')
+    #     email = self.normalize_email(email)
+    #     # user = self.model(email=email, **extra_fields)
+    #     user = self.model(email=email, is_staff=False, is_active=True, is_superuser=False, last_login=now, date_joined=now, **extra_fields)
+    #     user.set_password(password)
+    #     user.save(using=self._db)
+    #     return user
 
 
-    def create_user(self, email, password=None, **extra_fields):
+    # def create_user(self, email, password=None, **extra_fields):
         # extra_fields.setdefault('is_superuser', False)
         # return self._create_user(email, password, **extra_fields)
-        return self._create_user(email, password, False, False, **extra_fields)
+        # return self._create_user(email, password, False, False, **extra_fields)
 
+    def create_user(self,email,phone_number,password=None):
+        if not email:
+            raise ValueError("Users must enter an email address.")
+        if not phone_number:
+            raise ValueError("Users must enter a phone_number.")
+        user = self.model(email= self.normalize_email(email), phone_number = phone_number,)
+        user.set_password=password
+        user.save(using=self._db)
+        return user
 
     # def create_user(self, email, password=None, username=""):
     #   """
@@ -66,9 +75,7 @@ class UserManager(BaseUserManager):
     #   return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **kwargs):
-        # user = self.model(email=email, is_staff=True, is_admin=True, is_superuser=True, is_active=True, **kwargs)
-        # user = self.create_superuser(email=email, is_staff=True, is_superuser=True, is_active=True, **kwargs)
-        user = self.model(email=email, password=password, is_staff=True, is_superuser=True, is_active=True, **kwargs)
+        user = self.model(email=email, is_staff=True, is_admin=True, is_superuser=True, is_active=True, **kwargs)
         user.set_password(password)
         user.save(using=self._db)
         return user
--- accounts/views.py
+++ accounts/views.py
@@ -28,3 +28,57 @@ class MyLoginView(LoginView):
 
 
 login_view_modal = MyLoginView.as_view()
+
+
+def profile_detail(request, username):
+	titles = _('Member Profile')
+	# memberlist = Profile.objects.filter(member=request.user.id).values_list("id", flat=True).first()
+	get_fullname = Profile.objects.filter(username=request.user).values_list("full_name", flat=True).first()
+	# get_email = Profile.objects.filter(member=request.user).values_list("email", flat=True).first()
+	get_birthdate = Profile.objects.filter(username=request.user).values_list("birth_date", flat=True).first()
+	get_birthcity = Profile.objects.filter(username=request.user).values_list("birth_city", flat=True).first()
+
+	initial_dict = {
+		'full_name': get_fullname,
+		# 'birth_date': get_nataldate,
+		# 'birth_city': get_birth_city,
+		# 'longitude': get_city_longitude,
+		# 'latitude': get_city_latitude,
+		# 'member': memberlist,
+	}
+
+	if request.method == "POST":
+		form = ProfileForm(request.POST or None)
+
+		get_city_name = cache.get('get_city_name')
+		get_city_longitude = cache.get('get_city_longitude')
+		get_city_latitude = cache.get('get_city_latitude')
+
+		if form.is_valid():
+
+			profile = form.save(commit=False)
+			# profile.full_name = form.cleaned_data['full_name']
+			profile.full_name = form.cleaned_data.get('full_name')
+			# profile.birth_city = form.cleaned_data['birth_city']
+			profile.birth_city = form.cleaned_data.get('birth_city')
+			# profile.birth_date = form.cleaned_data['birth_date']
+			profile.birth_date = form.cleaned_data.get('birth_date')
+			profile.save()
+
+		else:
+			messages.warning(request, form.errors)
+	else:
+		form = ProfileForm(initial=initial_dict, instance=request.user)
+
+	context = {
+		'titles': titles,
+		'form': form,
+		'get_fullname': get_fullname,
+		# 'get_lastname': get_lastname,
+		# 'get_email': get_email,
+		'get_birthdate': get_birthdate,
+		'get_birthcity': get_birthcity,
+	}
+
+	return render(request, 'accounts/profile.html', context)
+
