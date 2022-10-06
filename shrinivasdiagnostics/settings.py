b3463b6569257d59ee6252d757cca8a56908178c
dj4va <dj4va@yahoo.com>
2022-10-06 07:16:19 +0700

    edit header and navbar

--- shrinivasdiagnostics/settings.py
+++ shrinivasdiagnostics/settings.py
@@ -240,30 +240,30 @@ if DEBUG:
     MIDDLEWARE += (
 		'debug_toolbar.middleware.DebugToolbarMiddleware',
 	)
-    TEMPLATES = [
-		{
-			'BACKEND': 'django.template.backends.django.DjangoTemplates',
-			'DIRS': [os.path.join(BASE_DIR, 'templates')],
-			'APP_DIRS': True,
-			'OPTIONS': 
-            {
-				"debug": DEBUG,
-				'context_processors': 
-                [
-					'django.template.context_processors.debug',
-					'django.template.context_processors.request',
-					'django.contrib.auth.context_processors.auth',
-					'django.template.context_processors.i18n',
-					'django.template.context_processors.media',
-					'django.template.context_processors.static',
-					'django.template.context_processors.tz',
-					'django.contrib.messages.context_processors.messages',
+    # TEMPLATES = [
+	# 	{
+	# 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+	# 		'DIRS': [os.path.join(BASE_DIR, 'templates')],
+	# 		'APP_DIRS': True,
+	# 		'OPTIONS': 
+    #         {
+	# 			"debug": DEBUG,
+	# 			'context_processors': 
+    #             [
+	# 				'django.template.context_processors.debug',
+	# 				'django.template.context_processors.request',
+	# 				'django.contrib.auth.context_processors.auth',
+	# 				'django.template.context_processors.i18n',
+	# 				'django.template.context_processors.media',
+	# 				'django.template.context_processors.static',
+	# 				'django.template.context_processors.tz',
+	# 				'django.contrib.messages.context_processors.messages',
 
-				],
-				"string_if_invalid": '<< MISSING VARIABLE "%s" >>' if DEBUG else "",
-			},
-		},
-	]
+	# 			],
+	# 			"string_if_invalid": '<< MISSING VARIABLE "%s" >>' if DEBUG else "",
+	# 		},
+	# 	},
+	# ]
 
     PAYMENT_VARIANTS = {
         'razorpay': 
