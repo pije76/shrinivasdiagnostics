0f18f52c6f15ffae473a69be9618ea8455a6418e
dj4va <dj4va@yahoo.com>
2022-10-19 09:05:22 +0700

    all in one

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,6 +14,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', login_view_modal, name='login_view_modal'),
+    path('', profile_detail, name='profile_detail'),
+    path('/login-form/', login_view_modal, name='login_view_modal'),
 ]
 
