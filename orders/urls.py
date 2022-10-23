bdd4ece4fd7c1ccaa76535c746de80cf68c2cda5
dj4va <dj4va@yahoo.com>
2022-10-23 17:52:44 +0700

    all in one commit

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+
+from .views import *
 
 app_name='orders'
 
 urlpatterns = [
-    path('create/',views.order_create,name='order_create'),
-    path('admin/order/<int:order_id>/',views.admin_order_detail,name="admin_order_detail"),
+    path('create/', order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name="admin_order_detail"),
 ]
